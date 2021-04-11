25ae36dc95281bd72f4d6ebf2cad7196ac875d5c
Imtiaz Miah <imtiazmiah@gmail.com>
2021-04-11 02:09:07 +0000

    finalizing survey

--- survey.js
+++ survey.js
@@ -5,12 +5,17 @@ const rl = readline.createInterface({
   output: process.stdout
 });
 
-let answers = []
 
 rl.question('What is your name or nickname? ', (name) => {
-  rl.question('What is your favourite food? ', (food) => {
-    console.log(name + " " + food);
-    rl.close();
+  rl.question('What is your superpower? ', (power) => {
+    rl.question('Who is your favourite musician or band? ', (band) => {
+      rl.question('What is your favourite pokemon? ', (pokemon) => {
+        rl.question('What would be your catchphrase? ', (phrase) => {
+          console.log(`Hi ${name}. Your super power is ${power}, and your catchphrase is "${phrase}". You seem to like music by ${band} and your favourite pokemon is ${pokemon}`);
+          rl.close()
+        });
+      });
+    });
   });
 });
 
